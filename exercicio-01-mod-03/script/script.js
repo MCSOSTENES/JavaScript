c4f6b0ebff1d2b35dcddbc4ddaf808ef129f49b1
Sóstenes Mesquita Chaves <77586595+MCSOSTENES@users.noreply.github.com>
2023-03-05 21:28:22 +0100

    day_001_atualizacao

--- exercicio-01-mod-03/script/script.js
+++ exercicio-01-mod-03/script/script.js
@@ -3,7 +3,7 @@
 function carregar(){
     // Variables declaration
     var msg = window.document.getElementById('msg')
-    var img = document.getElementById('img')
+    var img = document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
 
@@ -14,18 +14,21 @@ function carregar(){
 
     if (hora >= 0 && hora <=12){
         
-        img.setAttribute("style", "background-image: url('../img/morning.jpg')")
+        //img.setAttribute("style", "background-image: url('../img/morning.jpg')")
+        img.src = '../img/morning.png'
         document.body.style.background = '#e2cd9f'
 
     }
     else if (hora > 12 && hora <= 18){
         
-        img.setAttribute("style", "background-image: url('../img/afternoon.jpg')")
+        //img.setAttribute("style", "background-image: url('../img/afternoon.jpg')")
+        img.src = '../img/afternoon.png'
         document.body.style.background = '#b9846f'
 
     }
     else {
-        img.setAttribute("style", "background-image: url('../img/nigth.jpg')")
+        //img.setAttribute("style", "background-image: url('../img/nigth.jpg')")
+        img.src = '../img/nigth.png'
         document.body.style.background = '#515154'
     }
      
