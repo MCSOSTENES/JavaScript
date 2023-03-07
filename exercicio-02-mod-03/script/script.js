273e492edd470a9157d0eea3141e7f4b9e778c96
Sóstenes Mesquita Chaves <77586595+MCSOSTENES@users.noreply.github.com>
2023-03-07 01:08:02 +0100

    day_04

--- exercicio-02-mod-03/script/script.js
+++ exercicio-02-mod-03/script/script.js
@@ -16,15 +16,58 @@ function verificar(){
       
         var age = actualyear - Number(nascimento.value)
         var sex = document.getElementsByName('rdsex')
-        var gender = 'oi'
+        var gender = ''
         
         if(sex[0].checked){
             gender = 'Homem'
+            
+            if(age < 12){
+                
+                img.setAttribute('style', "background-image: url('../img/01-kid-m.jpg')")
+                
+            }
+            else if(age < 18){
+                             
+                img.setAttribute('style', "background-image: url('../img/02-ado-m.jpg')")
+            }
+            else if(age < 65){
+                             
+                img.setAttribute('style', "background-image: url('../img/03-adut-m.jpg')")
+            }
+            else if (age < 116){
+                             
+                img.setAttribute('style', "background-image: url('../img/04-old-m.jpg')")
+            }
+            else{
+                img.setAttribute('style', "background-image: url('../img/10-dead-m.jpg')")
+            }
+
         }
-        else (sex[1].checked){
-            gender = 'Mulher'
+        else {
+            gender = 'mulher'
+            
+            if(age < 12){
+                
+                img.setAttribute('style', "background-image: url('../img/05-kid-f.jpg')")
+            }
+            else if(age < 18){
+                             
+                img.setAttribute('style', "background-image: url('../img/06-ado-f.jpg')")
+            }
+            else if(age < 65){
+                             
+                img.setAttribute('style', "background-image: url('../img/07-adut-f.jpg')")
+            }
+            else if (age < 116){
+                             
+                img.setAttribute('style', "background-image: url('../img/08-old-f.jpg')")
+            }
+            else{
+                img.setAttribute('style', "background-image: url('../img/10-dead-f.jpg')")
+            }
         }
         
+        msg.style.textAlign = 'center'
         msg.innerHTML = (`É ${gender} com ${age} anos`)
 
     }
